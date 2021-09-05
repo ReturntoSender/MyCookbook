1e7ddbef0384883175d4f039d977b10cff6bf64d
ReturntoSender <TerraXF@gmail.com>
2021-09-05 22:30:03 +0200

    Adding bullets

--- Project_1_Alien_Invasion/settings.py
+++ Project_1_Alien_Invasion/settings.py
@@ -10,3 +10,10 @@ class Settings:
 
         # Schiffseinstellungen
         self.ship_speed = 1.5
+
+        # Geschosseinstellungen
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
